2cbe2fa66dcce750cfec03ad831ecdb747dc7062
Anjali-suresh-01 <88715091+Anjali-suresh-01@users.noreply.github.com>
2024-05-31 23:11:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let user = document.querySelector("#user");
+let login = document.querySelector(".login");
+
+user.addEventListener('click', () =>{
+    user.classList.toggle('fa-times');
+    login.classList.toggle('active');
+})
+
